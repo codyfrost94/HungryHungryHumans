ecb28a48ac0d9cc69cbdbe4e6a4dbe0be3a60bac
saschaschneider <s4schnei@gmail.com>
2017-10-02 20:06:53 -0700

    SS added firebase config

---
+++ assets/logic/logic.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDC8mMDrVv6YZX_fhhet5Css6o4MJ8IC_g",
+    authDomain: "hungryhungryhumans-44f29.firebaseapp.com",
+    databaseURL: "https://hungryhungryhumans-44f29.firebaseio.com",
+    projectId: "hungryhungryhumans-44f29",
+    storageBucket: "",
+    messagingSenderId: "473842520592"
+  };
+  firebase.initializeApp(config);
