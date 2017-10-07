2efb5ab1fb56785b48797969316ccd271e119d57
saschaschneider <s4schnei@gmail.com>
2017-10-07 13:58:09 -0700

    Added Firebase ingredient list functionality

--- assets/logic/ingredients.js
+++ assets/logic/ingredients.js
@@ -1,27 +1,33 @@
+ var config = {
+     apiKey: "AIzaSyDC8mMDrVv6YZX_fhhet5Css6o4MJ8IC_g",
+     authDomain: "hungryhungryhumans-44f29.firebaseapp.com",
+     databaseURL: "https://hungryhungryhumans-44f29.firebaseio.com",
+     projectId: "hungryhungryhumans-44f29",
+     storageBucket: "",
+     messagingSenderId: "473842520592"
+ };
+
 $(document).ready(function() {
     $(".delete-btn-1").hide();
     $(".delete-btn-2").hide();
 });
-// Initiialize firebase
-
-// var config = {
-//     apiKey: "AIzaSyDC8mMDrVv6YZX_fhhet5Css6o4MJ8IC_g",
-//     authDomain: "hungryhungryhumans-44f29.firebaseapp.com",
-//     databaseURL: "https://hungryhungryhumans-44f29.firebaseio.com",
-//     projectId: "hungryhungryhumans-44f29",
-//     storageBucket: "",
-//     messagingSenderId: "473842520592"
-// };
-// firebase.initializeApp(config);
-
-
 
 var database = firebase.database();
 
+firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+            console.log("Ingredients list " + user.uid);
+            var UID = user.uid;
+
+            var dataStuff = firebase.database().ref('Users/' + UID + '/ingrArray' );
+
+            dataStuff.on('value', function(snapshot) {
+
+                var ingrArray = [];
+                ingrArray = snapshot.val();
+
+
 //add ingredients to database
-
-
-var ingrArray = [];
 
 //var ingrInput = $("#user-ingredient").val();
 
@@ -44,13 +50,70 @@ $("#save-ingr-item").on("click", function(event) {
 
       //for (var i = 0; i < ingrInput.length; i++) {
       	
-      	console.log(ingrInput);
-      	//add list items
-      	var listItem = $("#ingredient-list").append('<li><input id="ingredient-checkbox" type="checkbox">' + ingrInput);
-      	ingrArray.push(ingrInput);
-      	console.log(ingrArray);
+            console.log(ingrInput);
+            //add list items
+            var listItem = $("#ingredient-list").append('<li><input id="ingredient-checkbox" type="checkbox">' + ingrInput);
+            ingrArray.push(ingrInput);
+            console.log(ingrArray);
 
-      	$("#user-ingredient").val("");
+
+            $("#user-ingredient").val("");
+
+            firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                console.log("Ingredients list " + user.uid);
+                var UID = user.uid;
+
+
+                firebase.database().ref('/Users/' + UID).update({
+                    ingrArray
+
+                })
+            }
+            })
+            })
+
+
+            })
+      }
+})
+
+
+    
+
+
+
+
+
+
+// function reset() {
+//     firebase.database().ref('/Users/' + rM1).update({
+//             p1Button: "enable",
+//             p2Button: "enable",
+//             buttonColor: "blue",
+//             anotherGame: false
+
+// })}
+
+
+
+// onAuthStateChanged(firebaseUser => {
+
+//      if (firebaseUser) {
+//          console.log(firebaseUser.uid);
+//          btnLogout.classList.remove('hide');
+//      } else {
+//          console.log("not logged in");
+//          btnLogout.classList.remove('hide');
+
+//      }
+
+//        pushedRef = firebase.database().ref().push({
+//                  uid: firebaseUser.uid 
+//              })
+              
+
+//  })
 
 
 
@@ -69,5 +132,5 @@ $("#save-ingr-item").on("click", function(event) {
 
 
 
-});
+
 
