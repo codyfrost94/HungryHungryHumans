bbd71b8d8105a7ce4faa049fcdf670960c8f1504
Michael Sanchez <futurefreq@hotmail.com>
2017-10-11 19:46:47 -0700

    fixed background and header

--- assets/logic/index-redirect.js
+++ assets/logic/index-redirect.js
@@ -1,4 +1,14 @@
-$("#btnLogin").on("click", function() {
-	window.location.href = "main.html";
-})	 
+//$("#btnLogin").on("click", function() {
+//	
+//	
+//	     const email = txtEmail.value;
+//     const pass = txtPassword.value;
+//     const auth = firebase.auth();
+//
+//     // Sign in
+//     const promise = auth.signInWithEmailAndPassword(email, pass);
+//     promise.catch(e => console.log(e.message));
+//     promise.catch(e => alert(e.message));
+//	window.location.href = "main.html";
+//})	 
 	
