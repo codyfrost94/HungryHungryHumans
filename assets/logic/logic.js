496d88980b1f5f0a97742426d1f3067112f4c2ba
Michael Sanchez <futurefreq@hotmail.com>
2017-10-14 13:16:41 -0700

    cleaned up final code

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -1,14 +1,12 @@
  var config = {
-     apiKey: "AIzaSyDC8mMDrVv6YZX_fhhet5Css6o4MJ8IC_g",
-     authDomain: "hungryhungryhumans-44f29.firebaseapp.com",
-     databaseURL: "https://hungryhungryhumans-44f29.firebaseio.com",
-     projectId: "hungryhungryhumans-44f29",
-     storageBucket: "",
-     messagingSenderId: "473842520592"
+ 	apiKey: "AIzaSyDC8mMDrVv6YZX_fhhet5Css6o4MJ8IC_g",
+ 	authDomain: "hungryhungryhumans-44f29.firebaseapp.com",
+ 	databaseURL: "https://hungryhungryhumans-44f29.firebaseio.com",
+ 	projectId: "hungryhungryhumans-44f29",
+ 	storageBucket: "",
+ 	messagingSenderId: "473842520592"
  };
  firebase.initializeApp(config);
-
-
 
  const txtEmail = document.getElementById('txtEmail');
  const txtPassword = document.getElementById('txtPassword');
@@ -18,66 +16,56 @@
 
  btnLogin.addEventListener('click', e => {
 
-     const email = txtEmail.value;
-     const pass = txtPassword.value;
-     const auth = firebase.auth();
+ 	const email = txtEmail.value;
+ 	const pass = txtPassword.value;
+ 	const auth = firebase.auth();
 
-     // Sign in
-     const promise = auth.signInWithEmailAndPassword(email, pass);
-     promise.catch(e => console.log(e.message));
-     promise.catch(e => alert(e.message));
-
-
-
+ 	// Sign in
+ 	const promise = auth.signInWithEmailAndPassword(email, pass);
+ 	promise.catch(e => console.log(e.message));
+ 	promise.catch(e => alert(e.message));
  });
 
  // Add signup event
 
  btnSignUp.addEventListener('click', e => {
 
-     // TODO: Check for real emailsz
+ 	// TODO: Check for real emailsz
 
-     const email = txtEmail.value;
-     const pass = txtPassword.value;
-     const auth = firebase.auth();
+ 	const email = txtEmail.value;
+ 	const pass = txtPassword.value;
+ 	const auth = firebase.auth();
 
-     // Sign in
-     const promise = auth.createUserWithEmailAndPassword(email, pass);
-     promise.catch(e => console.log(e.message));
-     promise.catch(e => alert(e.message));
+ 	// Sign in
+ 	const promise = auth.createUserWithEmailAndPassword(email, pass);
+ 	promise.catch(e => console.log(e.message));
+ 	promise.catch(e => alert(e.message));
 
-     var user = firebase.auth();
-     console.log("uid " + user.currentUser.uid);
+ 	var user = firebase.auth();
+ 	console.log("uid " + user.currentUser.uid);
 
-     firebase.database().ref('/Users/' + user.currentUser.uid).set({
-         'email': "blabla",
-         'address': "18440 hatteras st"
+ 	firebase.database().ref('/Users/' + user.currentUser.uid).set({
+ 		'email': "blabla",
+ 		'address': "18440 hatteras st"
 
-
-
-
-         //  firebase.database.ref('/Users/' +firebase.auth().currentUser.uid).set({
-         // 'email': email,
-         // 'address': "18440 hatteras st"
-
-     })
+ 	})
  });
 
  btnLogout.addEventListener('click', e => {
-     firebase.auth().signOut();
+ 	firebase.auth().signOut();
 
  });
 
  var user = {}
  var ohShit;
 
- $("#btnLogin").on("click", function() {
-     ohShit = true;
+ $("#btnLogin").on("click", function () {
+ 	ohShit = true;
  });
 
- $("#btnLogout").on("click", function() {
+ $("#btnLogout").on("click", function () {
 
-     window.location.href = "index.html";
+ 	window.location.href = "index.html";
  });
  // Add a realtime Listener
 
@@ -85,103 +73,83 @@
 
  firebase.auth().onAuthStateChanged(firebaseUser => {
 
-     if (firebaseUser) {
-         firebase.database().ref('Users/' + firebaseUser.uid + '/ingrArray').on('value', function(snapshot) {
-             console.log("user" + snapshot.val())
-             user = snapshot.val();
+ 	if (firebaseUser) {
+ 		firebase.database().ref('Users/' + firebaseUser.uid + '/ingrArray').on('value', function (snapshot) {
+ 			console.log("user" + snapshot.val())
+ 			user = snapshot.val();
 
-             console.log("I am logged in now");
+ 			console.log("I am logged in now");
 
-             // Disableing login & sign-up button as well as log-in forms when signed-in
-             $("#btnLogin").addClass('hide');
-             $("#btnSignUp").addClass('hide');
-             $("#txtEmail").addClass('hide');
-             $("#txtPassword").addClass('hide');
+ 			// Disableing login & sign-up button as well as log-in forms when signed-in
+ 			$("#btnLogin").addClass('hide');
+ 			$("#btnSignUp").addClass('hide');
+ 			$("#txtEmail").addClass('hide');
+ 			$("#txtPassword").addClass('hide');
 
-             if (ohShit) {
-                 window.location.href = "main.html";
-             }
+ 			if (ohShit) {
+ 				window.location.href = "main.html";
+ 			}
 
+ 			// Drag and drop functions
 
+ 			$(function () {
+ 				$(".draggable").draggable({
+ 					revert: "invalid"
+ 				});
 
-
-             //DRAGGABLE STUFF + ARRAY/////////////////////////////////////////////////////////
-             
-             $(function() {
-                 $(".draggable").draggable({
-                     revert: "invalid"
-                 });
-
-                 $(".list-item").on("mouseup", function(event) {
-                     var text = $(this).text();
-                     searchableIngr.push(text);
-                     console.log(searchableIngr);
-                 });
-                 //     var searchableIngr2 = [];
-                 $(".droppable-1, .droppable-2").on("drop", function(event) {
-                     $(this).append('<p class="list-item">' + searchableIngr[searchableIngr.length - 1] + '</p>');
-                     //         var text = $(this).find(".list-item").text();
-                     //         searchableIngr2.push(text);
-                     //         console.log(searchableIngr2);
-                 });
-                 $(".droppable-1, .droppable-2").droppable({
-                     drop: function(event, ui) {
-                         ui.draggable.remove();
-                         $(this).height(function(index, height) {
-                             return (height + 30);
-                         });
-                     }
-                 });
-             });
-             //DRAGGABLE STUFF + ARRAY/////////////////////////////////////////////////////////
-
-             // enabling logout button when signed-in
-             $("#btnLogout").removeClass('hide');
-
-             // adding message "Welcome user x" when user is logged-in
-             $("#userName").removeClass('hide');
+ 				$(".list-item").on("mouseup", function (event) {
+ 					var text = $(this).text();
+ 					searchableIngr.push(text);
+ 					console.log(searchableIngr);
+ 				});
+ 				//     var searchableIngr2 = [];
+ 				$(".droppable-1, .droppable-2").on("drop", function (event) {
+ 					$(this).append('<p class="list-item">' + searchableIngr[searchableIngr.length - 1] + '</p>');
+ 					//         var text = $(this).find(".list-item").text();
+ 					//         searchableIngr2.push(text);
+ 					//         console.log(searchableIngr2);
+ 				});
+ 				$(".droppable-1, .droppable-2").droppable({
+ 					drop: function (event, ui) {
+ 						ui.draggable.remove();
+ 						$(this).height(function (index, height) {
+ 							return (height + 30);
+ 						});
+ 					}
+ 				});
+ 			});
 
 
+ 			// enabling logout button when signed-in
+ 			$("#btnLogout").removeClass('hide');
 
+ 			// adding message "Welcome user x" when user is logged-in
+ 			$("#userName").removeClass('hide');
+ 		})
 
+ 		btnLogout.classList.remove('hide');
+ 	} else {
+ 		console.log("not logged in");
+ 		btnLogout.classList.remove('hide');
 
-         })
+ 		// Enabling login & sign-up button as well as log-in forms when signed-out
+ 		$("#btnLogin").removeClass('hide');
+ 		$("#btnSignUp").removeClass('hide');
+ 		$("#txtEmail").removeClass('hide');
+ 		$("#txtPassword").removeClass('hide');
 
+ 		// disabling logout button when signed-in
+ 		$("#btnLogout").addClass('hide');
 
+ 		// Disabling message "Welcome user x" when user is logged-in
+ 		$("#userName").addClass('hide');
 
-         btnLogout.classList.remove('hide');
-     } else {
-         console.log("not logged in");
-         btnLogout.classList.remove('hide');
-
-
-         // Enabling login & sign-up button as well as log-in forms when signed-out
-         $("#btnLogin").removeClass('hide');
-         $("#btnSignUp").removeClass('hide');
-         $("#txtEmail").removeClass('hide');
-         $("#txtPassword").removeClass('hide');
-
-         // disabling logout button when signed-in
-         $("#btnLogout").addClass('hide');
-
-         // Disabling message "Welcome user x" when user is logged-in
-         $("#userName").addClass('hide');
-
-
-         $(function() {
-             $(".draggable").draggable("disable")
-
-             //          .addClass( "ui-state-highlight" )
-             //          .find( "p" )
-             //            .html( "Dropped!" );
-         })
-
-     }
-     var uid = firebaseUser.uid;
-     pushedRef = firebase.database().ref('Users/' + uid).set({
-         ingrArray: ["First Item"]
-
-     })
-
-
+ 		$(function () {
+ 			$(".draggable").draggable("disable")
+ 		})
+ 	}
+ 	var uid = firebaseUser.uid;
+ 	pushedRef = firebase.database().ref('Users/' + uid).set({
+ 		ingrArray: ["First Item"]
+ 	})
  })
--- assets/logic/test.js
+++ assets/logic/test.js
@@ -1,262 +1,170 @@
- // var config = {
- //     apiKey: "AIzaSyDC8mMDrVv6YZX_fhhet5Css6o4MJ8IC_g",
- //     authDomain: "hungryhungryhumans-44f29.firebaseapp.com",
- //     databaseURL: "https://hungryhungryhumans-44f29.firebaseio.com",
- //     projectId: "hungryhungryhumans-44f29",
- //     storageBucket: "",
- //     messagingSenderId: "473842520592"
- // };
- // firebase.initializeApp(config);
- console.log("sup");
-
  // This is logic to create an array for the searchable recipe ingredients
  //depends on IDs so we need to make sure our IDs are all clean
 
  var tempIngr = [];
 
- $("#add-recipe-ingredient").on("click", function(event) {
-     event.preventDefault();
+ $("#add-recipe-ingredient").on("click", function (event) {
+ 	event.preventDefault();
 
-     tempIngr.push($("#recipe-ingredients").val().trim());
-     $("#recipe-ingredients").val("");
-     console.log(tempIngr);
-
+ 	tempIngr.push($("#recipe-ingredients").val().trim());
+ 	$("#recipe-ingredients").val("");
+ 	console.log(tempIngr);
  });
-
 
  function addRecipe() {
 
-     event.preventDefault();
+ 	event.preventDefault();
 
-     var recipeName = $("#create-recipe-title").val().trim();
-     var recipeInstructions = $("#instructions").val().trim();
-     var recipeCuisine = $("#create-cuisine-type").val().trim();
+ 	var recipeName = $("#create-recipe-title").val().trim();
+ 	var recipeInstructions = $("#instructions").val().trim();
+ 	var recipeCuisine = $("#create-cuisine-type").val().trim();
 
-     console.log("Ingredients: " + tempIngr);
+ 	console.log("Ingredients: " + tempIngr);
 
-     var newRecipe = {
-         name: recipeName,
-         cuisine: recipeCuisine,
-         ingredients: searchableIngr,
-         instructions: recipeInstructions,
-     }
+ 	var newRecipe = {
+ 		name: recipeName,
+ 		cuisine: recipeCuisine,
+ 		ingredients: searchableIngr,
+ 		instructions: recipeInstructions,
+ 	}
 
-     firebase.database().ref('/Recipes/').push(newRecipe);
+ 	firebase.database().ref('/Recipes/').push(newRecipe);
 
-     $("#create-recipe-title").val("");
-     $("#instructions").val("");
-     searchableIngr = []
-     $("#create-cuisine-type").val("");
+ 	$("#create-recipe-title").val("");
+ 	$("#instructions").val("");
+ 	searchableIngr = []
+ 	$("#create-cuisine-type").val("");
 
-
-
-     console.log(newRecipe);
+ 	console.log(newRecipe);
  }
 
-
- $("#add-recipe2").on("click", function(event) {
-
-     addRecipe();
-
+ $("#add-recipe2").on("click", function (event) {
+ 	addRecipe();
  });
 
- // var db = firebase.database();
- // var ref = db.ref("Recipes");
- // ref.orderByChild("name").equalTo("Test").on("child_added", function(snapshot) {
- //     console.log(snapshot.val());
- //     // console.log("this recipe is called " + snapshot.val().name + " and is made with " + snapshot.val().ingredients);
- // });
-
  function apiCall() {
-     var q = $("#recipe-title").val().trim();
-     var queryURL = "https://api.yummly.com/v1/api/recipes?_app_id=d930995b&_app_key=0478f2c3ea2c7a5234c78816e1d521c2&q=" + q + "&maxResult=10&start=10"
-     var displayDiv = $("#display-results-panel");
+ 	var q = $("#recipe-title").val().trim();
+ 	var queryURL = "https://api.yummly.com/v1/api/recipes?_app_id=d930995b&_app_key=0478f2c3ea2c7a5234c78816e1d521c2&q=" + q + "&maxResult=10&start=10"
+ 	var displayDiv = $("#display-results-panel");
 
-     $.ajax({
-         url: queryURL,
-         method: "GET"
-     }).done(function(response) {
-         console.log(queryURL);
-         console.log(response.matches);
+ 	$.ajax({
+ 		url: queryURL,
+ 		method: "GET"
+ 	}).done(function (response) {
+ 		console.log(queryURL);
+ 		console.log(response.matches);
 
-         for (i = 0; i < response.matches.length; i++) {
-             console.log(response.matches[i]);
-             var r = response.matches[i];
-             var rName = r.recipeName;
-             var rCourse = r.attributes.course;
-             var rCuisine = r.attributes.cuisine;
-             var rIngredients = r.ingredients
-             console.log(rName);
-             console.log(rIngredients);
-             console.log(rCourse);
-             console.log(rCuisine);
+ 		for (i = 0; i < response.matches.length; i++) {
+ 			console.log(response.matches[i]);
+ 			var r = response.matches[i];
+ 			var rName = r.recipeName;
+ 			var rCourse = r.attributes.course;
+ 			var rCuisine = r.attributes.cuisine;
+ 			var rIngredients = r.ingredients
+ 			console.log(rName);
+ 			console.log(rIngredients);
+ 			console.log(rCourse);
+ 			console.log(rCuisine);
 
-             var newDiv = $("<div class='panel panel-primary result'>");
-             var nameDiv = $("<div class = 'panel-heading'>");
-             var bodyDiv = $("<div class='panel-body'>");
-             var cuisineDiv = $("<h5>");
-             var ingrDiv = $("<div>");
+ 			var newDiv = $("<div class='panel panel-primary result'>");
+ 			var nameDiv = $("<div class = 'panel-heading'>");
+ 			var bodyDiv = $("<div class='panel-body'>");
+ 			var cuisineDiv = $("<h5>");
+ 			var ingrDiv = $("<div>");
 
-             cuisineDiv.text("Cuisine: " + rCuisine);
+ 			cuisineDiv.text("Cuisine: " + rCuisine);
 
-             for (i=0; i<rIngredients.length;i++) {
-             	var singleIngr = $("<p>");
-             	singleIngr.text(rIngredients[i]);
-             	ingrDiv.append(singleIngr);
-             }
+ 			for (i = 0; i < rIngredients.length; i++) {
+ 				var singleIngr = $("<p>");
+ 				singleIngr.text(rIngredients[i]);
+ 				ingrDiv.append(singleIngr);
+ 			}
 
-             nameDiv.text(rName);
-
-             bodyDiv.append(cuisineDiv, ingrDiv);
-             newDiv.append(nameDiv, bodyDiv);
-             displayDiv.append(newDiv);
-
-
-
-
-
-
-         }
-
-     });
-
+ 			nameDiv.text(rName);
+ 			bodyDiv.append(cuisineDiv, ingrDiv);
+ 			newDiv.append(nameDiv, bodyDiv);
+ 			displayDiv.append(newDiv);
+ 		}
+ 	});
  }
 
  function searchRecipe() {
 
-     $("#search-results").removeClass("hidden");
+ 	$("#search-results").removeClass("hidden");
+ 	event.preventDefault();
 
+ 	var displayDiv = $("#display-results-panel");
+ 	var searchName = $("#recipe-title").val().trim();
+ 	var searchIngr = searchableIngr
+ 	var db = firebase.database();
+ 	var ref = db.ref("Recipes");
+ 	ref.orderByChild("name").equalTo(searchName).on("child_added", function (snapshot) {
+ 		console.log("------------------------------")
+ 		console.log(snapshot.val());
+ 		console.log("------------------------------")
 
-     event.preventDefault();
+ 		var recipeName = snapshot.val().name;
+ 		var recipeIngr = snapshot.val().ingredients;
+ 		var recipeCuisine = snapshot.val().cuisine;
+ 		var recipeInstructions = snapshot.val().instructions;
 
-     var displayDiv = $("#display-results-panel");
-     var searchName = $("#recipe-title").val().trim();
-     var searchIngr = searchableIngr
-     var db = firebase.database();
-     var ref = db.ref("Recipes");
-     ref.orderByChild("name").equalTo(searchName).on("child_added", function(snapshot) {
-         console.log("------------------------------")
-         console.log(snapshot.val());
-         console.log("------------------------------")
+ 		console.log(recipeIngr);
 
-         var recipeName = snapshot.val().name;
-         var recipeIngr = snapshot.val().ingredients;
-         var recipeCuisine = snapshot.val().cuisine;
-         var recipeInstructions = snapshot.val().instructions;
+ 		var newDiv = $("<div class='panel panel-primary'>");
+ 		var nameDiv = $("<div class = 'panel-heading'>");
+ 		var bodyDiv = $("<div class='panel-body'>");
+ 		var cuisineDiv = $("<h5>");
+ 		var instructionsP = $("<p class = 'instr-p'>");
+ 		instructionsP.text(recipeInstructions);
+ 		cuisineDiv.text("Cuisine: " + recipeCuisine);
+ 		console.log("Cuisine Div: " + cuisineDiv.text());
+ 		nameDiv.text(recipeName);
 
-         console.log(recipeIngr);
+ 		console.log(newDiv);
 
-         var newDiv = $("<div class='panel panel-primary'>");
-         var nameDiv = $("<div class = 'panel-heading'>");
-         var bodyDiv = $("<div class='panel-body'>");
-         var cuisineDiv = $("<h5>");
-
-         // var ingrDiv = $("<div>");
-         // for (i=0; i<recipeIngr.length; i++){
-         // 	var z = $("h5");
-         // 	z.text(i);
-         // 	ingrDiv.append(z);
-
-         // 	console.log(i);
-         // };
-
-         var instructionsP = $("<p class = 'instr-p'>");
-         instructionsP.text(recipeInstructions);
-
-
-
-
-         cuisineDiv.text("Cuisine: " + recipeCuisine);
-         console.log("Cuisine Div: " + cuisineDiv.text());
-         nameDiv.text(recipeName);
-
-
-
-         console.log(newDiv);
-
-         bodyDiv.append(cuisineDiv, instructionsP);
-         newDiv.append(nameDiv, bodyDiv);
-         displayDiv.append(newDiv);
-
-
-
-
-         // var recipeDisplay = $("<div class='panel panel-primary'>");
-         // var recipeDisplayName = $("<div class='panel-heading'>".text(recipeName));
-         // recipeDisplay.append(recipeDisplay);
-         // displayDiv.append(recipeDisplay);
-
-
-
-         // console.log(recipeName);
-         // console.log(recipeIngr);
-         // for (i=0;i<recipeIngr.length;i++) {
-         // 	var ingrItem = recipeIngr[i];
-         // 	console.log("Ingredient number " + (i+1) + " is " + ingrItem);
-         // }
-         // console.log(recipeCuisine);
-         // console.log(recipeInstructions);
-         // console.log("------------------------------")
-
-
-
-     });
-
-
-
-
+ 		bodyDiv.append(cuisineDiv, instructionsP);
+ 		newDiv.append(nameDiv, bodyDiv);
+ 		displayDiv.append(newDiv);
+ 	});
  }
 
- $("#add-recipe").on("click", function(event) {
+ $("#add-recipe").on("click", function (event) {
 
-     $("#display-results-panel").empty();
+ 	$("#display-results-panel").empty();
 
-     searchRecipe();
-     apiCall();
-
-
+ 	searchRecipe();
+ 	apiCall();
  });
 
-
-
-
-
-
  var IngrJSON = [
-
-     {
-         name: "Noodles"
-
-     },
-     {
-         name: "Eggs"
+ 	{
+ 		name: "Noodles"
 
      },
-     {
-         name: "Butter"
+ 	{
+ 		name: "Eggs"
 
      },
-     {
-         name: "Bread"
+ 	{
+ 		name: "Butter"
 
      },
-     {
-         name: "Hot Sauce"
+ 	{
+ 		name: "Bread"
+
+     },
+ 	{
+ 		name: "Hot Sauce"
 
      },
  ]
 
-
  function addAllIngredients(ingredients) {
-     let ref = firebase.database().ref('ingredients')
-     ref.remove(function() {
-         for (var i = 0; i < ingredients.length; i++) {
-             firebase.database().ref('ingredients/' + ingredients[i].name).set(ingredients[i])
-
-         }
-     })
-
-
+ 	let ref = firebase.database().ref('ingredients')
+ 	ref.remove(function () {
+ 		for (var i = 0; i < ingredients.length; i++) {
+ 			firebase.database().ref('ingredients/' + ingredients[i].name).set(ingredients[i])
+ 		}
+ 	})
  }
-
  addAllIngredients(IngrJSON)
